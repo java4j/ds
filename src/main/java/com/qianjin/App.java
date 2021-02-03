15587243779dbd444bab056829d85c2993792dc0
fabruce <fabruce>
2021-02-03 20:52:32 +0800

    新增项目

---
+++ src/main/java/com/qianjin/App.java
@@ -0,0 +1,13 @@
+package com.qianjin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
