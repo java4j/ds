bde86c5a1008f9f06995beffd35897a42487d2cd
fabruce <fabruce>
2021-02-05 09:16:10 +0800

    新增链表算法

--- src/main/java/com/qianjin/ds/BinaryTree.java
+++ src/main/java/com/qianjin/ds/BinaryTree.java
@@ -4,7 +4,18 @@ package com.qianjin.ds;
  * 二叉树的各种操作
  */
 public class BinaryTree {
+    private Node root;
+    public void addNode() {
 
+    }
 
+    static class Node {
+        private int val;
+        private Node left;
+        private Node right;
 
+        public Node (int value) {
+            this.val = value;
+        }
+    }
 }
--- src/main/java/com/qianjin/ds/LinkList.java
+++ src/main/java/com/qianjin/ds/LinkList.java
@@ -26,13 +26,7 @@ public class LinkList {
         if (head == null) {
             head = node;
         } else {
-            Node prev = head;
-            Node next = prev.next;
-            while (next != null) {
-                prev = next;
-                next = prev.next;
-            }
-            prev.next = node;
+            head.addNext(node);
         }
         return this;
     }
@@ -43,9 +37,27 @@ public class LinkList {
      * @return
      */
     public void reverse() {
+        head = reverse(head);
+    }
+
+    /**
+     * 打印
+     */
+    public void print() {
+        print(head);
+    }
+
+    /**
+     * 链表反转静态方法
+     *
+     * @param head
+     * @return
+     */
+    public static Node reverse(Node head) {
         if (head == null || head.next == null) {
-            return;
+            return head;
         }
+        // 需要定义三个临时变量
         Node n1 = head;
         Node n2 = head.next;
         Node n3;
@@ -57,15 +69,18 @@ public class LinkList {
         }
         head.next = null;
         head = n1;
+        return head;
     }
 
     /**
-     * 打印
+     * 打印静态方法
+     *
+     * @param head
      */
-    public void print() {
+    public static void print(Node head) {
         Node next = head;
         while (next != null) {
-            System.out.print(next.a);
+            System.out.print(next.val);
             next = next.next;
             if (next != null) {
                 System.out.print("->");
@@ -74,26 +89,68 @@ public class LinkList {
         System.out.println();
     }
 
+    /**
+     * 合并两个有序的列表，使用递归的方式
+     *
+     * @return 合并后的头结点
+     */
+    public static Node mergeSequentialLists(Node n1, Node n2) {
+        if (n1 == null) return n2;
+        if (n2 == null) return n1;
+        Node head = null;
+        if (n1.val <= n2.val) {
+            head = n1;
+            head.next = mergeSequentialLists(n1.next, n2);
+        } else {
+            head = n2;
+            head.next = mergeSequentialLists(n1, n2.next);
+        }
+        return head;
+    }
+
     static class Node {
-        int a;
+        int val;
         Node next;
 
-        public Node(int a) {
-            this.a = a;
+        public Node(int val) {
+            this.val = val;
         }
 
-        public Node(int a, Node next) {
-            this.a = a;
+        public Node(int val, Node next) {
+            this.val = val;
             this.next = next;
+        }
+
+        public Node addNext(int val) {
+            Node node = new Node(val);
+            return addNext(node);
+        }
+
+        public Node addNext(Node node) {
+            Node prev = this;
+            Node next = prev.next;
+            while (next != null) {
+                prev = next;
+                next = prev.next;
+            }
+            prev.next = node;
+            return this;
         }
     }
 
 
     public static void main(String[] args) {
         LinkList linkList = new LinkList();
-        linkList.addNode(new Node(5)).addNode(new Node(7)).addNode(new Node(6));
+        linkList.addNode(new Node(5)).addNode(new Node(6)).addNode(new Node(7));
         linkList.print();
         linkList.reverse();
         linkList.print();
+
+        Node n1 = new Node(1);
+        n1.addNext(2).addNext(5).addNext(7);
+        Node n2 = new Node(3);
+        n2.addNext(4).addNext(6).addNext(9);
+        Node n = mergeSequentialLists(n1, n2);
+        print(n);
     }
 }
